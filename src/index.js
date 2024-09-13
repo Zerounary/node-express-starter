16e64e980072eff969051ac7fb02d728108753b9
宋诚 <lsjhtwl@163.com>
2024-09-13 15:24:20 +0800

    init project

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const HyperExpress = require('hyper-express');
+const webserver = new HyperExpress.Server();
+
+const api_v1_router = new HyperExpress.Router();
+const api_v2_router = new HyperExpress.Router();
+
+// Create GET route to serve 'Hello World'
+api_v1_router.get('/', (request, response) => {
+    response.send('version1');
+})
+
+
+api_v2_router.get('/', (request, response) => {
+    response.send('version2');
+})
+
+api_v2_router.get('/hi', (request, response) => {
+    response.send('hi2');
+})
+
+webserver.use('/api/v1', api_v1_router);
+webserver.use('/api/v2', api_v2_router);
+
+// Activate webserver by calling .listen(port, callback);
+const port = 90;
+webserver.listen(port)
+.then((socket) => console.log(`Webserver started on port ${port}`))
+.catch((error) => console.log(`Failed to start webserver on port ${port}`));
