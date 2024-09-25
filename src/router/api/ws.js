eef487aaf3792e8ed949cb4d8915802d885bdb1f
宋诚 <lsjhtwl@163.com>
2024-09-25 17:15:16 +0800

    feat: 完成 websocket 调用数据库

--- src/router/api/ws.js
+++ src/router/api/ws.js
@@ -1,8 +1,10 @@
-
 const HyperExpress = require("hyper-express");
 const logger = require("../../logger");
+const oracledb = require("oracledb");
 
 const ws_router = new HyperExpress.Router();
+
+let pool = {};
 
 // websocket
 ws_router.ws(
@@ -13,10 +15,49 @@ ws_router.ws(
   },
   async (ws) => {
     console.log(ws.ip + " is now connected using websockets!");
+    const send = (msg) => {
+      ws.send(JSON.stringify(msg));
+    };
+
     ws.on("close", () => console.log(ws.ip + " has now disconnected!"));
-    setTimeout(() => {
-        ws.send("Hello World!")
-    }, 1000)
+    ws.on("message", async (msg) => {
+      console.log(ws.ip + " has sent a message!");
+      console.log(msg);
+      let req = JSON.parse(msg);
+      console.log(req?.event);
+      if (pool[client_id]) {
+        let db = pool[client_id];
+        const res = await db.execute(req.sql);
+        let columns = res.metaData.map((meta) => ({
+          name: meta.name,
+          ty: meta.dbTypeName,
+          nullable: meta.nullable,
+        }));
+
+        send({
+            columns,
+            rows: res.rows,
+          })
+      }
+    });
+
+    let client_id = "123";
+
+    const conn = await oracledb.getConnection({
+      user: "bosnds3",
+      password: "abc123",
+      connectString: "app.burgeonerp.cn:22990/testorcl",
+    });
+
+    pool[client_id] = conn;
+
+    send({
+      event: "init",
+      data: {
+        client_id,
+        pool_size: Object.keys(pool).length,
+      },
+    });
   }
 );
 
--- tests/wsc.js
+++ tests/wsc.js
@@ -1,24 +1,32 @@
-const WebSocket = require('ws')
+const WebSocket = require("ws");
 
-const url = 'ws://localhost:90/ws/connect'
+const url = "ws://localhost:90/ws/connect";
 
 const ws = new WebSocket(url, {
-  perMessageDeflate: false
+  perMessageDeflate: false,
 });
 
 const send = (data) => {
-  ws.send(data)
-}
+  ws.send(JSON.stringify(data));
+};
 
-ws.on('open', function open() {
-    console.log(`${url} connected`);
-    setTimeout(() => {
-        ws.close()
-    }, 5000)
-})
+ws.on("open", function open() {
+  console.log(`${url} connected`);
+  // setTimeout(() => {
+  //     ws.close()
+  // }, 5000)
+});
 
-ws.on('error', console.error);
+ws.on("error", console.error);
 
-ws.on('message', function message(data) {
-  console.log('received: %s', data);
+ws.on("message", function message(data) {
+  let resp = JSON.parse(data);
+  console.log("received: ", resp);
+  if(resp.event == 'init') {
+    send({
+        event: 'query',
+        clent_id: resp.data.client_id,
+        sql: 'select sysdate from dual'
+    })
+  }
 });
