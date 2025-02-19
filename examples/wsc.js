2a41126effc769fc6f8d3c6bd4324e44db3f64fa
宋诚 <lsjhtwl@163.com>
2025-02-19 17:29:12 +0800

    feat: 支持 sql 查询

--- examples/wsc.js
+++ examples/wsc.js
@@ -33,32 +33,49 @@ ws.on("message", function message(data) {
   console.log("received: ", data.toString());
   let resp = JSON.parse(data);
   if (resp.event == "init") {
-    let target_id = "8cafa326-0c89-4885-b7b6-44b6365e1647"; // BPM服务
-    // let target_id = '1abad9f6-79b1-4862-9d6e-db0d5a542f61'  // 开发环境
-    // send({
-    //     target_id: '1abad9f6-79b1-4862-9d6e-db0d5a542f61',
-    //     api: "query",
-    //     params: {
-    //       sql: 'select sysdate from dual'
-    //     }
-    // })
-    download(target_id)
+    let target_id = "34230b42-ad9f-42de-8e7d-a5fdd0a179a2"; // BPM服务
+    // let target_id = "1abad9f6-79b1-4862-9d6e-db0d5a542f61"; // 开发环境
+    // let target_id = '51898dc3-d000-4a33-b02b-668f910c8090';
+
+    db_query(target_id);
   } else if (resp.event == "data") {
     console.log("received: ", resp);
   }
 });
+
+const db_query = (target_id) => {
+  send({
+    target_id,
+    api: "query",
+    params: {
+      sql: "select t.id,t.description, t.name, t.emp_name, t.store_sql, t.is_unsubmit, t.creationdate, t.c_store_filter from mb_users t where t.id = 161",
+    },
+  });
+};
+
+const dir_lookup = (target_id) => {
+  send({
+    target_id,
+    api: "dir_lookup",
+    params: {
+      // dir 为空是 默认查看 apps下有哪些文件
+      // dir: '.', // .表示相对路径, 如何 "./apps/image-compress"
+      dir: "D:/",
+    },
+  });
+};
 
 const download = (target_id) => {
   send({
     target_id,
     api: "download",
     params: {
-        // url: 'http://localhost:910/image-compress.zip',
-        url: 'http://localhost:910/config.zip',
-        extract_dir: 'apps/image-compress',
+      // url: 'http://localhost:910/image-compress.zip',
+      url: "http://localhost:910/config.zip",
+      extract_dir: "apps/image-compress",
     },
   });
-}
+};
 
 const install_app = (target_id) => {
   send({
@@ -68,7 +85,7 @@ const install_app = (target_id) => {
       app_name: "image-compress",
     },
   });
-}
+};
 
 const start_app = (target_id) => {
   send({
@@ -78,7 +95,7 @@ const start_app = (target_id) => {
       app_name: "image-compress",
     },
   });
-}
+};
 
 const stop_app = (target_id) => {
   send({
