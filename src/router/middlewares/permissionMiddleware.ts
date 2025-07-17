a155d22b8eb67645c210547f3c2c27043008f773
宋诚 <lsjhtwl@163.com>
2025-07-17 11:43:07 +0800

    todo: 新增优化点

--- src/router/middlewares/permissionMiddleware.ts
+++ src/router/middlewares/permissionMiddleware.ts
@@ -10,6 +10,7 @@ export const checkPermission = (action: string) => {
         // Replace placeholders like :tableName with actual values from params
         const finalAction = action.replace(/:(\w+)/g, (match, key) => req.params[key] || match);
 
+        // TODO 这个需要优化，避免每次都查询数据库
         const hasPerm = await PermissionService.hasPermission(req.user.id, finalAction);
         if (!hasPerm) {
            return res.status(403).json(fail(`Forbidden: Missing permission for action: ${finalAction}`));
