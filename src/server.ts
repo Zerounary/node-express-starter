06abb4677663e1b78fbd425d4ab746bca28fd498
宋诚 <lsjhtwl@163.com>
2025-08-25 16:30:14 +0800

    feat: 新增 benchmark 参考测试接口

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,12 @@ import webserver from "./app";
 
 export function start() {
     const port = 80; //process.env.PORT || 22987;
+    webserver.get('/benchmark', async (req, res) => {
+      return res.send('ok');
+    })
+    webserver.get('/benchmark2', (req, res) => {
+      return res.send('ok');
+    })
     webserver
       .listen(port)
       .then(() => {
